d2a60fe7058d72f0def083eef7113096fa2b502c
Tane <tzvorskaya18@gmail.com>
2023-07-05 16:48:04 +0300

    Update

--- market/products/admin.py
+++ market/products/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 
-from products.models import ProductsCategory, Products
+from products.models import ProductsCategory, Products, Basket
 
 admin.site.register(ProductsCategory)
-admin.site.register(Products)
+
+@admin.register(Products)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('image', 'name', 'description', ('price', 'quantity'), 'stripe_product_price_id', 'category')
+    readonly_fields = ('description',)
+    search_fields = ('name',)
+    ordering = ('-name',)
+
+
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity', 'created_timestamp')
+    readonly_fields = ('created_timestamp',)
+    extra = 0
