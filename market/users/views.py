73a381f3b43d348aa070d6700f6ba859bd925e7c
Tane <tzvorskaya18@gmail.com>
2023-07-08 17:48:28 +0300

    Update

--- market/users/views.py
+++ market/users/views.py
@@ -1,40 +1,62 @@
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
 from django.urls import reverse, reverse_lazy
+from django.views.generic.base import TemplateView
 from django.contrib.auth.decorators import login_required
 from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.messages.views import SuccessMessageMixin
 
+from common.views import TitleMixin
 from users.models import User
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from products.models import Basket
 
 
-def login(request):
-    if request.method == 'POST':
-        form = UserLoginForm(data=request.POST)
-        if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
-            user = auth.authenticate(username=username, password=password)
-            if user:
-                auth.login(request, user)
-                return HttpResponseRedirect(reverse('index'))
-    else:
-        form = UserLoginForm()
-    context = {'form': form}
-    return render(request, 'users/login.html', context)
+class UserLoginView(TitleMixin, LoginView):
+    template_name = 'users/login.html'
+    form_class = UserLoginForm
+    title = 'Store - Авторизация'
 
 
-class UserRegistrationView(CreateView):
+class UserRegistrationView(TitleMixin, SuccessMessageMixin, CreateView):
     model = User
     form_class = UserRegistrationForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')
+    success_message = 'Регистрация прошла успешно.'
+    title = 'Store - Регистрация'
+
+
+class UserProfileView(TitleMixin, UpdateView):
+    model = User
+    form_class = UserProfileForm
+    template_name = 'users/profile.html'
+    title = 'Store - Личный кабинет'
+
+    def get_success_url(self):
+        return reverse_lazy('user:profile', args=(self.object.id,))
 
     def get_context_data(self, **kwargs):
-        context = super(UserRegistrationView, self).get_context_data()
-        context['title'] = 'Store - Регистрация'
+        context = super(UserProfileView, self).get_context_data()
+        context['baskets'] = Basket.objects.filter(user=self.object)
         return context
+
+
+# def login(request):
+#     if request.method == 'POST':
+#         form = UserLoginForm(data=request.POST)
+#         if form.is_valid():
+#             username = request.POST['username']
+#             password = request.POST['password']
+#             user = auth.authenticate(username=username, password=password)
+#             if user:
+#                 auth.login(request, user)
+#                 return HttpResponseRedirect(reverse('index'))
+#     else:
+#         form = UserLoginForm()
+#     context = {'form': form}
+#     return render(request, 'users/login.html', context)
 
 
 # def registration(request):
@@ -50,20 +72,6 @@ class UserRegistrationView(CreateView):
 #     context = {'form': form}
 #     return render(request, 'users/register.html', context)
 
-
-class UserProfileView(UpdateView):
-    model = User
-    form_class = UserProfileForm
-    template_name = 'users/profile.html'
-
-    def get_success_url(self):
-        return reverse_lazy('user:profile', args=(self.object.id,))
-
-    def get_context_data(self, **kwargs):
-        context = super(UserProfileView, self).get_context_data()
-        context['title'] = 'Store - Личный кабинет'
-        context['baskets'] = Basket.objects.filter(user=self.object)
-        return context
 
 # @login_required
 # def profile(request):
@@ -83,6 +91,6 @@ class UserProfileView(UpdateView):
 #     return render(request, 'users/profile.html', context)
 
 
-def logout(request):
-    auth.logout(request)
-    return HttpResponseRedirect(reverse('index'))
+# def logout(request):
+#     auth.logout(request)
+#     return HttpResponseRedirect(reverse('index'))
